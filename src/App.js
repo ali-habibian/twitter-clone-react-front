b138bdb7ea5f682acdeea6a2ab8c7127a9435462
Ali Habibian <ali.habibian04@gmail.com>
2023-10-02 19:09:40 +0330

    Add routing between Auth and Home

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Authentication from './Components/Authentication/Authentication';
+import HomePage from './Components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="">
+
+      <Routes>
+        <Route path="" element={true ? <HomePage /> : <Authentication />}></Route>
+      </Routes>
 
     </div>
   );
