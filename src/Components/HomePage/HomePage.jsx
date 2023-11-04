a88fa6f3950c5490555787af5ee3446c4210c18c
Ali Habibian <ali.habibian04@gmail.com>
2023-11-04 17:24:14 +0330

    implement RightSection

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import Navigation from '../Navigation/Navigation'
 import HomeSection from '../HomeSection/HomeSection'
+import RightSection from '../RightSection/RightSection'
 
 const HomePage = () => {
     return (
@@ -11,12 +12,12 @@ const HomePage = () => {
                 <Navigation />
             </Grid>
 
-            <Grid item xs={12} lg={6} className='hidden lg:block w-full relative'>
+            <Grid item xs={12} lg={6} className='px-5 lg:px-9 hidden lg:block w-full relative'>
                 <HomeSection />
             </Grid>
 
             <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
-                <p className='text-center'>Right Part</p>
+                <RightSection />
             </Grid>
 
         </Grid>
