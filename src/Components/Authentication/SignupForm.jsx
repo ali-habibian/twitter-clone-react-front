56133d64a2bf234d021d5ac11cad7154d604857c
Ali Habibian <ali.habibian04@gmail.com>
2023-12-06 15:21:50 +0330

    feat: Implement user registration in SignupForm component
    This commit adds the ability to register users in the SignupForm component. The form is configured to use formik and Yup for validation. When the form is submitted, the values are processed, and the date of birth is formatted before dispatching a 'registerUser' action using Redux. The 'registerUser' action sends a POST request to the authentication endpoint, registering the user with the provided information.
    
    In the `SignupForm.jsx` file, a new function `handleSubmit` is added to handle form submission. It extracts the day, month, and year from the dateOfBirth field, formats the date, and dispatches the 'registerUser' action.
    
    The `Auth/Action.js` file is updated to include the 'registerUser' action, which sends a POST request to the authentication endpoint with the user registration data.
    
    This enhancement allows users to register through the SignupForm, contributing to the overall functionality of the authentication process.

--- src/Components/Authentication/SignupForm.jsx
+++ src/Components/Authentication/SignupForm.jsx
@@ -2,7 +2,9 @@ import { Button, Grid, InputLabel, MenuItem, Select, TextField } from '@mui/mate
 import { blue } from '@mui/material/colors'
 import { useFormik } from 'formik'
 import React from 'react'
+import { useDispatch } from 'react-redux'
 import * as Yup from 'yup'
+import { registerUser } from '../../Store/Auth/Action'
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().email("Invalid email").required("Email is required"),
@@ -10,7 +12,7 @@ const validationSchema = Yup.object().shape({
 })
 
 const SignupForm = () => {
-
+  const dispatch = useDispatch();
   const currentYear = new Date().getFullYear();
   const years = Array.from({ length: 100 }, (_, i) => currentYear - i);
   const days = Array.from({ length: 31 }, (_, i) => i + 1);
@@ -45,6 +47,8 @@ const SignupForm = () => {
       const { day, month, year } = values.dateOfBirth
       const dateOfBirth = `${year}-${month}-${day}`
       values.dateOfBirth = dateOfBirth
+
+      dispatch(registerUser(values))
       console.log(values)
     }
   })
